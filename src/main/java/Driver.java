94965bc906d0ad1aebf78a425b6cdac197a28207
Nitesh Bhargava <nitesh.bhargv@gmail.com>
2024-09-29 01:48:56 +0530

    task scheduler

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,14 +1,28 @@
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Driver {
 
     public static void main(String[] args) throws InterruptedException {
-        TaskScheduler taskScheduler = new TaskScheduler();
         Task task = new Task("one");
 
+        Set<Thread> allThreads = new HashSet<Thread>();
+        final TaskScheduler deferredCallbackExecutor = new TaskScheduler();
+
+        Thread service = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    deferredCallbackExecutor.start();
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        });
+        service.start();
         Task task2 = new Task("two", new Date().getTime() + 10000);
-        taskScheduler.scheduleTask(task);
-        taskScheduler.scheduleTask(task2);
+        deferredCallbackExecutor.scheduleTask(task);
+        deferredCallbackExecutor.scheduleTask(task2);
 
     }
 }
--- src/main/java/TaskScheduler.java
+++ src/main/java/TaskScheduler.java
@@ -4,30 +4,60 @@ import java.util.PriorityQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TaskScheduler {
 
     PriorityQueue<Task> priorityQueue;
-
+    ReentrantLock lock = new ReentrantLock();
+    Condition newCallbackArrived = lock.newCondition();
     ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     public TaskScheduler() {
         this.priorityQueue = new PriorityQueue<>(Comparator.comparing(Task::getDate));
     }
 
-    public void scheduleTask(Task myTask) throws InterruptedException {
-        if(myTask!=null) {
-            priorityQueue.add(myTask);
-            System.out.println("Adding task: "+ myTask.getName());
+    public void start() throws InterruptedException {
+        long sleepFor = 0;
+        while (true) {
+
+            lock.lock();
+
+            while (priorityQueue.size() == 0) {
+                newCallbackArrived.await();
+            }
+
+            while (priorityQueue.size() != 0) {
+                sleepFor = findSleepDuration();
+
+                if(sleepFor <=0)
+                    break;
+
+                newCallbackArrived.await(sleepFor, TimeUnit.MILLISECONDS);
+            }
+
+            Task cb = priorityQueue.poll();
+            System.out.println(
+                    "Executed at " + System.currentTimeMillis()/1000 + " required at " + cb.getDate()/1000
+                            + ": message:" + cb.name);
+            cb.run();
+
+            lock.unlock();
         }
-        while(!priorityQueue.isEmpty() && priorityQueue.peek().getDate() <= new Date().getTime()) {
-            Task task = priorityQueue.poll();
-            executor.execute(task);
-        }
-        if(priorityQueue.isEmpty()) {
-            return;
-        } else {
-            TimeUnit.MICROSECONDS.sleep(priorityQueue.peek().getDate()-new Date().getTime());
-            scheduleTask(null);
-        }
+
+    }
+
+
+
+    private long findSleepDuration() {
+        long currentTime = System.currentTimeMillis();
+        return priorityQueue.peek().getDate() - currentTime;
+    }
+
+    public void scheduleTask(Task task) {
+        lock.lock();
+        priorityQueue.add(task);
+        newCallbackArrived.signal();
+        lock.unlock();
     }
 }
